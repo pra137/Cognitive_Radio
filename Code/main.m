cdfa12ef4beab1718aa805d180a3e2be861ef440
Vaddadi V K Praneeth <pra137@gmail.com>
2017-09-25 14:24:47 +0200

    Code changes

--- Code/main.m
+++ Code/main.m
@@ -1,3 +1,10 @@
 %Primary User Parameters
-seq_length = 10000;
-mod_type = 16;
+PU_length = 10000;
+PU_mod = 16;
+PU_seq = 2;
+
+%Secondary User Parameters
+SU_length = 2000;
+SU_mod = 4;
+SU_seq = 2;
+
